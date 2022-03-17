d6600cc12e75e6afc455a09450e108994aadc2ea
OzoneClub <cryptokuinka@gmail.com>
2022-03-17 08:37:52 +0000

    change port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
